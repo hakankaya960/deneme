6a398f84e343c8a086283f266a6d10548a3280c3
aliyilmaz375 <aliyilmaz9182@gmail.com>
2020-11-24 16:30:33 +0300

    message

--- src/deneme/Deneme01.java
+++ src/deneme/Deneme01.java
@@ -4,12 +4,13 @@ public class Deneme01 {
 
 	public static void main(String[] args) {
 		System.out.println("Birinci Hakan");
-    System.out.println("Ikinci Hakan");
+		System.out.println("Ikinci Hakan");
 		
 		
 		
 		System.out.println("1. ali branch");
-		
+		System.out.println("2. ali");
+		System.out.println("3. ali");
 
 
 		
